05025ff2b74070d6f30e9e1f42ada870efb9ee97
Johannes van Heerden <johannescvh@icloud.com>
2025-07-25 09:21:00 +0200

    Initial project files.

---
+++ CSharpDSA/DataStructures/List.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructsAndAlgos.DataStructures;
+public class List<T> 
+{
+    T[] Data;
+
+    public List(T Type)
+    {
+
+    }
+}
