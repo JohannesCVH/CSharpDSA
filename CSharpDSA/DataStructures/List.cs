2926af361a6a98ef1c0c26b82c83641e6a6c27da
Johannes van Heerden <johannescvh@icloud.com>
2025-07-26 19:37:21 +0200

    Added basic operations to the List data structure.

--- CSharpDSA/DataStructures/List.cs
+++ CSharpDSA/DataStructures/List.cs
@@ -1,16 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DataStructsAndAlgos.DataStructures;
 
-namespace DataStructsAndAlgos.DataStructures;
-public class List<T> 
+public class List<T>
 {
-    T[] Data;
+    private T?[] Data;
+    private int LastValueIdx;
 
-    public List(T Type)
+    public T this[uint i]
     {
+        get => Data[i]!;
+        set => Data[i] = value;
+    }
 
+    public List()
+    {
+        Data = Array.Empty<T>();
+        LastValueIdx = -1;
+    }
+
+    public List(T val) : this()
+    {
+        Data[0] = val;
+        LastValueIdx = 0;
+    }
+
+    public void Add(T val)
+    {
+        int idx = LastValueIdx + 1;
+        if (idx > Data.Length - 1)
+        {
+            ResizeData(true);
+        }
+        Data[idx] = val;
+        LastValueIdx++;
+    }
+
+    private void ResizeData(bool bigger)
+    {
+        if (bigger)
+        {
+            T?[] DataTemp = Data;
+            Data = new T?[Data.Length == 0 ? 1 : Data.Length * 2];
+            Array.Copy(DataTemp, Data, DataTemp.Length);
+        }
+    }
+
+    public void Print(bool withValues = true)
+    {
+        Console.WriteLine("\n---- List ----");
+        Console.WriteLine("Length: {0}", Data.Length);
+        Console.WriteLine("Last value index: {0}", LastValueIdx);
+
+        if (withValues)
+        {
+            Console.WriteLine("Values");
+            Console.WriteLine("--------------");
+            for (int i = 0; i < LastValueIdx + 1; i++)
+            {
+                Console.WriteLine(Data[i]);
+            }
+        }
+
+        Console.WriteLine("--------------\n");
     }
 }
